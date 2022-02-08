80f39c3156383dad2000d4b1c85fa1c48b36a3c0
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-02-07 22:27:06 -0300

    chore: add @testing-library/react-hooks

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  testPathIgnorePatterns: [ 
+    "/node_modules",
+    "/android",
+    "/ios"
+  ],
   transformIgnorePatterns: ['/node_modules/(?!native-base)/'],
   verbose: true,
 };
